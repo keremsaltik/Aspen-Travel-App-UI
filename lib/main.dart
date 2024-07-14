5f7aceb9f974c45150b0f06e8755f54fc6ddc8e8
Kerem <keremsaltikbusiness@gmail.com>
2024-07-14 09:50:51 +0300

    lastCommit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        bottomNavigationBarTheme:
+            BottomNavigationBarThemeData(backgroundColor: Colors.white),
+        cardTheme: CardTheme(color: Colors.white),
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
